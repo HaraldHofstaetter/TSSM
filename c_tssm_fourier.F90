046bd12f6be05e2d18295c45392d86b7fbeed9e5
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2016-02-01 13:42:09 +0100

    added c_set_fftw_planning_rigor subroutine

--- c_tssm_fourier.F90
+++ c_tssm_fourier.F90
@@ -7,4 +7,9 @@ contains
         call initialize_tssm_fourier
     end subroutine c_initialize_tssm_fourier 
 
+    subroutine c_set_fftw_planning_rigor(flag) bind(c)
+        integer(c_int), value :: flag
+        fftw_planning_rigor = flag
+    end subroutine c_set_fftw_planning_rigor
+
 end module c_tssm_fourier
